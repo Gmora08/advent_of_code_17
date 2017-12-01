a6711c022b46d9670c32ef655461b01d4af0b05c
Gustavo <gmora008@gmail.com>
2017-12-01 15:40:46 -0600

    initial commit

---
+++ lib/advent_of_code_17.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCode17 do
+  @moduledoc """
+  Documentation for AdventOfCode17.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode17.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/advent_of_code_17_test.exs
@@ -0,0 +1,8 @@
+defmodule AdventOfCode17Test do
+  use ExUnit.Case
+  doctest AdventOfCode17
+
+  test "greets the world" do
+    assert AdventOfCode17.hello() == :world
+  end
+end
